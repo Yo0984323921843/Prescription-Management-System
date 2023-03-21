c5e8934e0897b39406a45ef2e95eb04c092abb2c
Your Name <you@example.com>
2023-03-21 14:32:34 +0000

    added icons

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Prescription-Management-System
+download code from git
+use following command
+npm i
+npm run make
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow,ipcMain } = require('electron')
+// const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+const url = require('url')
 const path = require('path')
-
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -9,10 +10,17 @@ const createWindow = () => {
         preload: path.join(__dirname, 'preload.js'),
       },
   })
+  const mainMenu = Menu.buildFromTemplate([{}]);
+Menu.setApplicationMenu(mainMenu);
   ipcMain.handle('ping', () => 'pong')
-  win.loadFile('index.html')
+  win.loadFile('./renderer/index.html')
+  
 }
 
 app.whenReady().then(() => {
+  // const displays = screen.getAllDisplays()
+  // console.log('ele ',displays
+  // )
+
   createWindow()
 })
