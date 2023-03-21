944fec97622707a244761d33767e5ee8b52f249a
Your Name <you@example.com>
2023-03-21 23:33:29 +0000

    Created Login page

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-const { app, BrowserWindow,ipcMain } = require('electron')
+const { app, BrowserWindow,ipcMain, Menu } = require('electron')
 // const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 const url = require('url')
 const path = require('path')
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: '80vh',
+    height: '95vh',
     webPreferences: {
         preload: path.join(__dirname, 'preload.js'),
       },
   })
-  const mainMenu = Menu.buildFromTemplate([{}]);
+  const mainMenu = Menu.buildFromTemplate([{
+    label:"Quit",
+    accelerator: process.platform == 'darwin'?'command+Q':'Ctrl+Q',
+    click(){
+      app.quit();
+    }
+  }]);
 Menu.setApplicationMenu(mainMenu);
   ipcMain.handle('ping', () => 'pong')
-  win.loadFile('./renderer/index.html')
+  win.loadFile('./src/index.html')
   
 }
 
